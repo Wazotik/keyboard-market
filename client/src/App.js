53f09581bf9f30c5aa874d636f81dbba26a2db7c
Wahaj <wahajhaider32@gmail.com>
2021-12-26 02:37:04 -0500

    added navbar and a sidebar that appears after clicking icon in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,129 +1,17 @@
 import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-import ProductCard from "./components/product-card";
+import Keyboards from "./pages/keyboards.js";
+import Navbar from "./components/navbar.js";
+import { Route, Routes } from "react-router-dom";
 
-const axios = require("axios");
+
 
 function App() {
-	const [productElemList, setProductElemList] = useState([]);
-	const [productInfo, setProductInfo] = useState([]);
-	const [infoLoaded, setInfoLoaded] = useState(false);
-
-	// const [productsInCart, setProductsInCart] = useState([]);
-
-	const updateProductInfo = async () => {
-		const res = await axios.get("/scraped-product-info");
-		setInfoLoaded(true);
-		const info = res.data;
-		setProductInfo(res.data);
-
-		setProductElemList(
-			info.map((product) => {
-				return (
-					<ProductCard
-						name={product.name}
-						imgUrl={product.img}
-						price={product.price}
-					></ProductCard>
-				);
-			})
-		);
-	};
-
-	const sortPriceHigh = () => {
-		const sortedProducts = productInfo.sort((a, b) => {
-			if (Number(a.price.substring(1) > Number(b.price.substring(1)))) {
-				return -1;
-			} else if (
-				Number(a.price.substring(1) < Number(b.price.substring(1)))
-			) {
-				return 1;
-			} else {
-				return 0;
-			}
-		});
-
-		setProductElemList(
-			sortedProducts.map((product) => {
-				return (
-					<ProductCard
-						name={product.name}
-						imgUrl={product.img}
-						price={product.price}
-					></ProductCard>
-				);
-			})
-		);
-	};
-
-	const sortPriceLow = () => {
-		const sortedProducts = productInfo.sort((a, b) => {
-			if (Number(a.price.substring(1) < Number(b.price.substring(1)))) {
-				return -1;
-			} else if (
-				Number(a.price.substring(1) > Number(b.price.substring(1)))
-			) {
-				return 1;
-			} else {
-				return 0;
-			}
-		});
-
-		setProductElemList(
-			sortedProducts.map((product) => {
-				return (
-					<ProductCard
-						name={product.name}
-						imgUrl={product.img}
-						price={product.price}
-					></ProductCard>
-				);
-			})
-		);
-	};
-
-	useEffect(() => {
-		updateProductInfo();
-	}, []);
-
 	return (
 		<div>
-			<div className={styles.title}>
-				<h1>Discover new keyboards!</h1>
-			</div>
-
-			<div className={styles.buttonsContainer}>
-				<div>
-					<button onClick={sortPriceHigh}>
-						Sort by highest price
-					</button>
-				</div>
-				<div>
-					<button onClick={sortPriceLow}>Sort by lowest price</button>
-				</div>
-			</div>
-
-			<div
-				style={{
-					display: infoLoaded ? "none" : "grid",
-					placeContent: "center",
-					height: "80vh",
-					textAlign: "center",
-					fontFamily: "Poppins",
-					margin: "0",
-				}}
-			>
-				<h2>loading keyboard data...</h2>
-				<img
-					src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
-					alt=""
-					style={{
-						width: "50%",
-						margin: "0 auto",
-					}}
-				/>
-			</div>
-			<div className={styles.productList}>{productElemList}</div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Keyboards />}></Route>
+			</Routes>
 		</div>
 	);
 }
---
+++ client/src/components/navbar.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import styles from "./navbar-style.module.css";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import { IoMdArrowBack } from "react-icons/io";
+
+const Navbar = () => {
+
+	const [showSidebar, setShowSidebar] = useState(false);
+
+
+
+	return (
+		<div>
+			<div className={styles.navbar}>
+				<FaBars className={styles.sidebarMenuIcon} size={28} onClick={() => {
+					setShowSidebar(!showSidebar);
+				}}/>
+			</div>
+			
+			<nav className={showSidebar ? styles.navMenuActive : styles.navMenuHidden}>
+				<ul className={styles.items}>
+					<li>
+						<IoMdArrowBack className={styles.sidebarBackIcon} size={36} onClick={() => {
+							setShowSidebar(!showSidebar);
+						}}/>
+					</li>
+					<div className={styles.pageLinks}>
+						<Link to="/">Keyboard Catalog</Link>
+						<Link to="/">Your keyboards</Link>
+					</div>
+				</ul>
+			</nav>
+
+		</div>
+	);
+};
+
+export default Navbar;
