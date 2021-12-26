607f38848f7663f1bb68f962136aa9a17a0a1b2d
Wahaj <wahajhaider32@gmail.com>
2021-12-26 01:04:05 -0500

    added top fading-in button to each product container

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,13 @@ const axios = require("axios");
 function App() {
 	const [productElemList, setProductElemList] = useState([]);
 	const [productInfo, setProductInfo] = useState([]);
+	const [infoLoaded, setInfoLoaded] = useState(false);
 
 	// const [productsInCart, setProductsInCart] = useState([]);
 
 	const updateProductInfo = async () => {
 		const res = await axios.get("/scraped-product-info");
+		setInfoLoaded(true);
 		const info = res.data;
 		setProductInfo(res.data);
 
@@ -85,12 +87,41 @@ function App() {
 	}, []);
 
 	return (
-		<div className="App">
-			<div>
-				<button onClick={sortPriceHigh}>Sort by highest price</button>
+		<div>
+			<div className={styles.title}>
+				<h1>Discover new keyboards!</h1>
 			</div>
-			<div>
-				<button onClick={sortPriceLow}>Sort by lowest price</button>
+
+			<div className={styles.buttonsContainer}>
+				<div>
+					<button onClick={sortPriceHigh}>
+						Sort by highest price
+					</button>
+				</div>
+				<div>
+					<button onClick={sortPriceLow}>Sort by lowest price</button>
+				</div>
+			</div>
+
+			<div
+				style={{
+					display: infoLoaded ? "none" : "grid",
+					placeContent: "center",
+					height: "80vh",
+					textAlign: "center",
+					fontFamily: "Poppins",
+					margin: "0",
+				}}
+			>
+				<h2>loading keyboard data...</h2>
+				<img
+					src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
+					alt=""
+					style={{
+						width: "50%",
+						margin: "0 auto",
+					}}
+				/>
 			</div>
 			<div className={styles.productList}>{productElemList}</div>
 		</div>
--- client/src/components/product-card.js
+++ client/src/components/product-card.js
@@ -1,27 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./product-card-styles.module.css";
 
 const ProductCard = ({ name, imgUrl, price }) => {
-
-	const [showButton, setShowButton ] = useState(false);
-
-
+	const [showButton, setShowButton] = useState(false);
 
 	return (
-		<div onMouseEnter={() => {
-			setShowButton(true);
-		}} onMouseLeave={() => {
-			setShowButton(false);
-		}} className={styles.card}>
+		<div
+			onMouseEnter={() => {
+				setShowButton(true);
+			}}
+			onMouseLeave={() => {
+				setShowButton(false);
+			}}
+			className={styles.card}
+		>
 			<div className={styles.imgContainer}>
 				<img src={imgUrl} alt="" />
 			</div>
 			<div className={styles.name}>{name}</div>
 			<div className={styles.price}>{price}</div>
 
-			<button style={showButton ? {opacity: '100', height: '3rem', transition: 'all 0.3s ease-in-out'} : {opacity: '0', height: '0', transition: 'all 0.3s ease-in-out', margin: '0'}}>Add to Cart</button>
-
-
+			<button style={{
+				opacity: showButton ? "1" : "0",
+				transform: showButton ? "translateY(1rem)" : "translateY(0rem)",
+			}} className={styles.addButton}>Add To Cart</button>
 		</div>
 	);
 };
