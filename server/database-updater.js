46d6150fa1ea0aeaec8bf950302a33492dddbdcd
Wahaj <wahajhaider32@gmail.com>
2023-08-30 02:35:39 -0400

    change cron schedule update database every Sunday

--- server/database-updater.js
+++ server/database-updater.js
@@ -2,8 +2,8 @@ import * as dataScraper from"./data-scraper.js";
 import { insertKeyboards } from "./database.js";
 import cron from "node-cron";
 
-// Update database with new scraped keyboard info every 1st day of the month
-cron.schedule("0 0 1 * *", async () => {
+// Update database with new scraped keyboard info every Sunday
+cron.schedule("0 0 * * 0", async () => {
 	try {
 		const scrapedKeyboardData = await dataScraper.scrapeProducts();
 		await insertKeyboards(scrapedKeyboardData);
