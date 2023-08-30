730f513c55590f5b2e91e0747672c4561993c8e0
Wahaj <wahajhaider32@gmail.com>
2023-08-30 05:22:34 -0400

    changing cron schedule timing for testing purposes

--- server/database-updater.js
+++ server/database-updater.js
@@ -2,13 +2,13 @@ import * as dataScraper from"./data-scraper.js";
 import { updateKeyboards } from "./database.js";
 import cron from "node-cron";
 
-// Update database with new scraped keyboard info every half hour
-cron.schedule("*/5 * * * *", async () => {
+// Update database with new scraped keyboard info every 5 minutes hour
+cron.schedule("* * * * * *", async () => {
 	try {
 		console.log("updating keyboard info in DB");
-		const scrapedKeyboardData = await dataScraper.scrapeProducts();
-		await updateKeyboards(scrapedKeyboardData);
-		console.log("Data scraped and saved to DB successfully :)");
+		// const scrapedKeyboardData = await dataScraper.scrapeProducts();
+		// await updateKeyboards(scrapedKeyboardData);
+		// console.log("Data scraped and saved to DB successfully :)");
 	} catch (error) {
 		console.error("Error with Database updater", error);
 	}
