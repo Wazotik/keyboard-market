047e0ee432b18af82f19a8cd9240f43655f1c877
Wahaj <wahajhaider32@gmail.com>
2023-08-30 05:26:00 -0400

    reversting cron schedule changes

--- server/database-updater.js
+++ server/database-updater.js
@@ -3,12 +3,12 @@ import { updateKeyboards } from "./database.js";
 import cron from "node-cron";
 
 // Update database with new scraped keyboard info every 5 minutes hour
-cron.schedule("* * * * * *", async () => {
+cron.schedule("*/5 * * * *", async () => {
 	try {
 		console.log("updating keyboard info in DB");
-		// const scrapedKeyboardData = await dataScraper.scrapeProducts();
-		// await updateKeyboards(scrapedKeyboardData);
-		// console.log("Data scraped and saved to DB successfully :)");
+		const scrapedKeyboardData = await dataScraper.scrapeProducts();
+		await updateKeyboards(scrapedKeyboardData);
+		console.log("Data scraped and saved to DB successfully :)");
 	} catch (error) {
 		console.error("Error with Database updater", error);
 	}
