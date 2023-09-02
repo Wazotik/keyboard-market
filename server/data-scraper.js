fd15992c7ae8829154da4ea3d78774893b4454ee
Wahaj <wahajhaider32@gmail.com>
2023-09-02 03:43:02 -0400

    added the ability for the data scraper to scrape a product's star rating, larger image, first 10 reviews with their dates (done by implementing 2D array where each element is an array with the review and the date it was posted as its 2 elements)

--- server/data-scraper.js
+++ server/data-scraper.js
@@ -13,7 +13,7 @@ export const scrapeProducts = async () => {
 
 		let allPagesKeyboardObjs = [];
 
-		for (let i = 1; i <= 12; i++) {
+		for (let i = 1; i <= 1; i++) {
 			console.log(`Page: ${i}`);
 			let url = `https://mechanicalkeyboards.com/shop/index.php?pg=${i}&l=product_list&c=1&show=100`;
 			await page.goto(url, { waitUntil: "domcontentloaded" });
@@ -36,7 +36,8 @@ export const scrapeProducts = async () => {
 						img: `https://mechanicalkeyboards.com/shop/${product
 							.querySelector(".ebj_limit_img_height")
 							.getAttribute("src")}`,
-						price: product.querySelector(".sale").innerText,
+						price: product.querySelector(".sale").innerHTML,
+						productID: product.querySelector("input[type='hidden']").getAttribute("value"),
 					};
 					productObjs.push(obj);
 					console.log("product info added");
@@ -47,6 +48,57 @@ export const scrapeProducts = async () => {
 			allPagesKeyboardObjs = allPagesKeyboardObjs.concat(singlePageKeyboardObjs);
 		}
 
+		for (let i = 0; i < allPagesKeyboardObjs.length; i++) {
+			let productObj = allPagesKeyboardObjs[i];
+			console.log(`fetching data for product id: ${productObj.productID}`);
+			let url = `https://mechanicalkeyboards.com/shop/index.php?l=product_detail&p=${productObj.productID}`
+
+			await page.goto(url, { waitUntil: "domcontentloaded" });
+			console.log("went to url successful");
+			await page.waitForSelector(".product-info");
+			await page.waitForSelector(".review-list");
+			let largerImgUrl = await page.evaluate(() => {
+				let productInfoElem = document.querySelector(".product-info");
+				let imgUrl = `https://mechanicalkeyboards.com/shop/${productInfoElem.querySelector("img").getAttribute("src")}`;
+				return imgUrl;
+			})
+			productObj["largerImg"] = largerImgUrl; 
+
+			let allReviewsData = await page.evaluate(() => {
+				const allReviewElems = document.querySelectorAll(".review-content");
+				
+				// Limit on reviews reduce amount of storage used
+				let numberOfReviewsToMap = 10;
+
+				if (allReviewElems.length !== 0) {
+					let reviewsData = ([...allReviewElems].slice(0, numberOfReviewsToMap)).map((reviewElem) => {
+						let reviewText = reviewElem.querySelector("p").innerText; 
+						let reviewDate = (reviewElem.previousElementSibling).querySelector("span").innerText
+						return [reviewText, reviewDate];
+					})
+					console.log(reviewsData);
+					return reviewsData;
+				}
+				else {
+					return [];
+				}
+			})
+			productObj["reviewsData"] = allReviewsData;
+
+			let starRating = await page.evaluate(() => {
+				let productMetadataElem = document.querySelector(".review-metadata");
+				if (productMetadataElem) {
+					let rating = productMetadataElem.querySelector("img").getAttribute("src").charAt(13);
+					return rating;
+				}
+				else {
+					return -1;
+				}
+			})
+			productObj["starRating"] = starRating;
+		}
+
+
 		await browser.close();
 		console.log(allPagesKeyboardObjs);
 		return allPagesKeyboardObjs;
@@ -54,3 +106,5 @@ export const scrapeProducts = async () => {
 		console.log(error);
 	}
 };
+
+scrapeProducts();
